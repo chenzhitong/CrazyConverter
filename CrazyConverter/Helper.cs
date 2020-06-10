dda454d4ae34391a9e1de81d45b553fdea9b0bfc
陈志同 <chenzhitong12@hotmail.com>
2020-06-10 15:25:40 +0800

    update

--- CrazyConverter/Helper.cs
+++ CrazyConverter/Helper.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Neo3ConversionTool
+namespace CrazyConverter
 {
     public static class Helper
     {
