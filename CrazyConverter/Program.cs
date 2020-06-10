ee3386f9af379cbb1784bcd6089ac3715981feed
陈志同 <chenzhitong12@hotmail.com>
2020-06-10 15:02:26 +0800

    添加项目文件。

---
+++ CrazyConverter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CrazyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine(Helper.StringToBase64String(input));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Exception");
+                }
+            }
+        }
+    }
+}
